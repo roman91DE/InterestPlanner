767f4e7a9654de7f1221c74e9b51bc7db5bf80f9
rohoehn <rohoehn123@gmail.com>
2021-02-12 19:24:00 +0100

    finished_v1

--- tablewindow.cpp
+++ tablewindow.cpp
@@ -6,12 +6,22 @@ tableWindow::tableWindow(std::shared_ptr <financial_plan> f, QWidget *parent)
     : QTableWidget(parent), finance_plan(f)
 {
     int cols = 5;
-    int rows = this->finance_plan->get_totalTime()+1;
+
+    int rows = this->finance_plan->get_totalTime();
     this->setRowCount(rows);
     this->setColumnCount(cols);
-    this->setGeometry(0,0,600,600);
-
-    this->show();
+    this->setGeometry(0,0,3000,2000);
+    const QStringList header_horizontal = { "Periode", "Tilgung", "Zinszahlungen", "Annuität", "Restliche Schuld" };
+    this->setHorizontalHeaderLabels(header_horizontal);
+    std::vector < std::vector <double>> data = finance_plan->get_matrix();
+    for (int i = 0; i < rows; i++) {
+        for (int k = 0; k < cols; k++) {
+            QTableWidgetItem *temp = new QTableWidgetItem(QString::number(data[i][k]));
+            temp->setTextAlignment(Qt::AlignCenter);
+            this->setItem(i,k,temp);
+        }
+    }
+    this->showFullScreen();
 }
 
 tableWindow::~tableWindow() {
